8397cff577922709653f49a7d20d864c1d25b460
renancvitor <renan.vitor.cm@gmail.com>
2025-05-17 11:35:48 -0300

    Identificando os top 5 melhores episódios

--- src/main/java/com/renan/javaspring/principal/Principal.java
+++ src/main/java/com/renan/javaspring/principal/Principal.java
@@ -7,8 +7,10 @@ import com.renan.javaspring.service.ConsumoApi;
 import com.renan.javaspring.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -46,5 +48,16 @@ public class Principal {
 //        }
         
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episódios:");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
