64c8c9afb3648bfafff3e4dfd087eb437d88f4ab
renancvitor <renan.vitor.cm@gmail.com>
2025-05-17 10:05:31 -0300

    Buscando dados completos da série

--- src/main/java/com/renan/javaspring/JavaspringApplication.java
+++ src/main/java/com/renan/javaspring/JavaspringApplication.java
@@ -1,16 +1,9 @@
 package com.renan.javaspring;
 
-import com.renan.javaspring.model.DadosEpisodio;
-import com.renan.javaspring.model.DadosSerie;
-import com.renan.javaspring.model.DadosTemporada;
-import com.renan.javaspring.service.ConsumoApi;
-import com.renan.javaspring.service.ConverteDados;
+import com.renan.javaspring.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class JavaspringApplication implements CommandLineRunner {
@@ -21,27 +14,7 @@ public class JavaspringApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ConsumoApi consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?apikey=1653b8ce&t=Supernatural");
-//		System.out.println(json);
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=1653b8ce&t=Supernatural&Season=5&Episode=3");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i <= dados.totalTemporadas(); i++) {
-			json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=1653b8ce&t=Supernatural&Season=" + i);
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
--- src/main/java/com/renan/javaspring/principal/Principal.java
+++ src/main/java/com/renan/javaspring/principal/Principal.java
@@ -1,13 +1,19 @@
 package com.renan.javaspring.principal;
 
+import com.renan.javaspring.model.DadosSerie;
+import com.renan.javaspring.model.DadosTemporada;
 import com.renan.javaspring.service.ConsumoApi;
+import com.renan.javaspring.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner scan = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
 
     private final String API_KEY = "1653b8ce";
     private final String ENDERECO = "https://www.omdbapi.com/?apikey=" + API_KEY + "&t=";
@@ -16,6 +22,21 @@ public class Principal {
         System.out.println("Digite o nom e da série para buscar:");
         var nomeSerie = scan.nextLine();
         var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+"));
+
+        conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +
+                    "&Season=" + i);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
+
         // https://www.omdbapi.com/?apikey=1653b8ce&t=Supernatural&Season=5&Episode=3
     }
 }
