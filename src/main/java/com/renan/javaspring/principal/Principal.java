1c1485aaf99c63c658cb01e801188c280c490238
renancvitor <renan.vitor.cm@gmail.com>
2025-05-17 23:47:48 -0300

    Exibindo as etapas de uma stream

--- src/main/java/com/renan/javaspring/principal/Principal.java
+++ src/main/java/com/renan/javaspring/principal/Principal.java
@@ -57,11 +57,16 @@ public class Principal {
                 .flatMap(t -> t.episodios().stream())
                 .collect(Collectors.toList());
 
-        System.out.println("\nTop 5 episódios:");
+        System.out.println("\nTop 10 episódios:");
         dadosEpisodios.stream()
                 .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .peek(e -> System.out.println("Primeiro filtro(N/A) " + e))
                 .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-                .limit(5)
+                .peek(e -> System.out.println("Ordenação " + e))
+                .limit(10)
+                .peek(e -> System.out.println("Limite " + e))
+                .map(e -> e.titulo().toUpperCase())
+                .peek(e -> System.out.println("Mapeamento " + e))
                 .forEach(System.out::println);
 
         List<Episodio> episodios = temporadas.stream()
