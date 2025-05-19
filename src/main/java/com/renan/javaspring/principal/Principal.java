bc240d5e389a276fb853332d6f6bf951df4470b5
renancvitor <renan.vitor.cm@gmail.com>
2025-05-18 21:10:03 -0300

    Criando um mapa com dados por temporada

--- src/main/java/com/renan/javaspring/principal/Principal.java
+++ src/main/java/com/renan/javaspring/principal/Principal.java
@@ -100,5 +100,11 @@ public class Principal {
                                 " Episódio: " + e.getTitulo() +
                                 " Data lançamento: " + e.getDataLancamento().format(dateTimeFormatter)
                 ));
+
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 8.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));
+        System.out.println(avaliacoesPorTemporada);
     }
 }
