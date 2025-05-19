dd3d0c22e60e14e45b98dbe1bb5209c323a1afab
renancvitor <renan.vitor.cm@gmail.com>
2025-05-18 22:20:14 -0300

    Preparação desafio

---
+++ src/main/java/com/renan/javaspring/apiTabelaFipe/FipeSpringApplication.java
@@ -0,0 +1,21 @@
+package com.renan.javaspring.apiTabelaFipe;
+
+import com.renan.javaspring.JavaspringApplication;
+import com.renan.javaspring.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeSpringApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaspringApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal();
+        principal.exibeMenu();
+    }
+}
